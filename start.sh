c7e3075c9b2ef00ee2dafd3a24a2fae60c181b38
Kris Kelley <kelleyblackmore@gmail.com>
2018-12-16 18:46:50 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,10 @@
+as sudo
+check os 
+  if red hat
+    yum
+  if deb
+   apt-get -y install python3-pip
+
 if red hat
   subscription-manager register --username <username> --password <password> --auto-attach
-  sudo apt-get -y install python3-pip
+  
