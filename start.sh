182248f0d4a414ba093b5ac92084a36b40e374f1
Kris Kelley <kelleyblackmore@gmail.com>
2019-05-19 11:57:39 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,22 +6,9 @@ cd /usr/bin
 source /etc/environment && export PATH
 terraform --version
 
+# Install ansible
+sudo yum install ansible
 
-if [[ "$OSTYPE" == "linux-gnu" ]]; then
-       sudo yum install ansible
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-       pip install ansible
-elif [[ "$OSTYPE" == "cygwin" ]]; then
-        # POSIX compatibility layer and Linux environment emulation for Windows
-elif [[ "$OSTYPE" == "msys" ]]; then
-        # Lightweight shell and GNU utilities compiled for Windows (part of MinGW)
-elif [[ "$OSTYPE" == "win32" ]]; then
-        # I'm not sure this can happen.
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-       sudo apt-get install software-properties-common
-       sudo apt-add-repository ppa:ansible/ansible
-       sudo apt-get update
-       sudo apt-get install ansible
 
 
 
