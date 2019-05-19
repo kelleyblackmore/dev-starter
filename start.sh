3776cfb3e69df747b78eb32f8e3e87f252020c4f
Kris Kelley <kelleyblackmore@gmail.com>
2019-05-19 11:59:25 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
-wget https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip
-unzip terraform_0.11.11_linux_amd64.zip 
+wget https://releases.hashicorp.com/terraform/0.11.14/terraform_0.11.14_linux_amd64.zip
+unzip terraform_0.11.14_linux_amd64.zip
 sudo mv terraform /usr/bin/terraform
 export PATH="$PATH:/usr/bin/terraform"
 cd /usr/bin
